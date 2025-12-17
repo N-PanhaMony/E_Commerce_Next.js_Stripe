15a3fe51219fb1b00590162bd6a3b9cf58377bd7
N-PanhaMony <neangpanhamony001@gmail.com>
2025-12-17 22:48:54 +0700

    add initial product structure

--- components/ImageBanner.jsx
+++ components/ImageBanner.jsx
@@ -1,5 +1,37 @@
+'use client'
+
+import { useEffect, useRef, useState } from "react"
+
 export default function ImageBanner(){
+
+    const [isLoaded , setIsLoaded] = useState(false)
+    const imgRef = useRef()
+    
+    useEffect(()=>{
+        if(imgRef.current.complete){
+            setIsLoaded(true)
+        }
+    },[])
+
     return(
-        <div></div>
+        <div className="banner-img">
+            <img className="low-res-img" src="low_res/banner.jpeg" alt="banner-low-res" />
+            <img ref={imgRef} className="high-res-img" src="high_res/banner.png" alt="banner-high-res" style={ {opacity: isLoaded ? 1 : 0}} onLoad={() =>{
+                // the callback function executed when the high-res completed loaded
+                setIsLoaded(true)
+            }}/>
+            <div className="cta-btns-container">
+                <div>
+                    <div>
+                        <h3>Welcome to</h3>
+                        <h1>the EStore</h1>
+                    </div>
+                    <div>
+                        <button>Shop........</button>
+                        <button>Shop........</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
--- components/Products.jsx
+++ components/Products.jsx
@@ -1,5 +1,74 @@
+'use client'
+
 export default function Products(){
+
+    const Description = {
+        Docker: "aaaaaaaa",
+        data2: "aaaaaaaa",
+        data3: "aaaaaaaa",
+        data4: "aaaaaaaa",
+        data5: "aaaaaaaa",
+        data6: "aaaaaaaa",
+        data7: "aaaaaaaa",
+        data8: "aaaaaaaa",
+        data9: "aaaaaaaa",
+
+    }
+
+    const ArrDes = Object.keys(Description)
     return(
-        <div></div>
+        <>
+            <div className="section-container">
+                <div className="sectionn-header">
+                    <h2>Shop Our Seletion</h2>
+                    <p>Accessorization</p>
+                </div>
+
+                <div className="planner-content">
+                    <div>
+                        <button className="img-button">
+                            <img src="low_res/planner.jpeg" alt="high-res-planner"/>
+                        </button>
+                    </div>
+                    <div className="planner-info">
+                        <p className="text-large planner-header">Product header</p>
+                        <h3><span>$</span>9.99</h3>
+                        <p>Description</p>
+                        <ul>
+                            <li>title</li>
+                            <li>info</li>
+                        </ul>
+                    </div>
+                    <div className="purchase-btns">
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="section-container">
+                <div className="sectionn-header">
+                    <h2>Collect Your Favorite</h2>
+                    <p>custom design tech</p>
+                </div>
+                <div className="sticker-container">
+                    {ArrDes.map((s,sIndex) => {
+                        return(
+                            <div key={sIndex} className="card">
+                                <button className="img-button">
+                                    <img src={`low_res/${s}.jpeg`} alt={`${s}-low-res`}/>
+                                </button>
+                                <div className="s-info">
+                                    <p className="text-medium">{s.replaceAll('_',' ')} <sticker className="png"></sticker> </p>
+                                    <p>{Description[s]}</p>
+                                    <h4><span>$</span>9.99</h4>
+                                    <button>Add to Cart</button>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+
+        </>
     )
 }
