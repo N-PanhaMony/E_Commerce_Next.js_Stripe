b668eb49c432e0106a5e28ef90baea1e7629092a
N-PanhaMony <neangpanhamony001@gmail.com>
2025-12-28 21:33:55 +0700

    fix url for production

--- app/page.js
+++ app/page.js
@@ -3,9 +3,8 @@ import Products from "@/components/Products";
 
 // Function to fetch all products from the API route
 export async function getProducts() {
-  const baseURL = process.env.NEXT_PUBLIC_BASE_URL
-  
-  const response = await fetch(baseURL +'/api/products')
+  const baseURL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
+  const response = await fetch(`${baseURL}/api/products`)
   const products = await response.json()
   return products
 }
